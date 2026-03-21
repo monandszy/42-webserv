c8bf6863ccdf507071865bc80d50e7a2417402d6
Debian <monandszy@tuta.io>
2026-03-21 20:21:13 +0100

    feat: keep-alive multi request in buffer loop logic

--- server/request_handler.cpp
+++ server/request_handler.cpp
@@ -45,31 +45,37 @@ In case of POST do not close, read based on Content-Length
 int request_handler::process_request(Client& client) {
   char tmp_buffer[RECV_SIZE] = {0};
   ssize_t bytes = recv(client.getFd(), tmp_buffer, sizeof(tmp_buffer) - 1, 0);
-a
+
   if (bytes <= 0) return 1;
   client.append(tmp_buffer, bytes);
 
-  if (client.getStatus() == READING_HEADERS) {
-    size_t end_pos = client.getBuffer().find(HTTP_END);
-    if (end_pos != std::string::npos) process_headers(client, end_pos);
+  while (1) {
+    if (client.getStatus() == READING_HEADERS) {
+      size_t end_pos = client.getBuffer().find(HTTP_END);
+      if (end_pos != std::string::npos) {
+        process_headers(client, end_pos);
+      }
+    }
+
+    if (client.getStatus() == READING_BODY) {
+      if (client.getBuffer().size() >= client.getRequest().getBodySize()) {
+        process_body(client);
+      }
+    }
+
+    if (client.getStatus() == READY_TO_RESPOND) {
+      std::cout << "Fully parsed " << client.getRequest().getMethod()
+                << " request for " << client.getRequest().getUri() << "\n";
+
+      const char* resp = "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK";
+      send(client.getFd(), resp, strlen(resp), 0);
+
+      if (false) {  // TODO keep alive
+        client.reset();
+        continue;
+      }  // there could be another request in the buffer waiting for processing
+      return 1;
+    }
+    return 0;
   }
-
-  if (client.getStatus() == READING_BODY) {
-    if (client.getBuffer().size() >= client.getRequest().getBodySize())
-      process_body(client);
-  }
-
-  if (client.getStatus() == READY_TO_RESPOND) {
-    std::cout << "Fully parsed " << client.getRequest().getMethod()
-              << " request for " << client.getRequest().getUri() << "\n";
-
-    const char* resp = "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK";
-    send(client.getFd(), resp, strlen(resp), 0);
-
-    // if keep alive
-    // client.reset();
-    // return 0;
-    return 1;
-  }
-  return 0;
 }
